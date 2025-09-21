511c4ee041e3788738ed9e02264e6ff0349c4d5c
Sidharth <sidharthacker@gmail.com>
2025-09-21 12:08:38 +0530

    add friend function

--- Abstraction/Abstractio.cpp
+++ Abstraction/Abstractio.cpp
@@ -1,3 +1,8 @@
+//.........................Virtual function.....................
+
+// A virtual function is a member function in a base class
+//that can be overridden in a derived class.
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -18,3 +23,36 @@ int main(){
     c1.draw();
     return 0;
 }
+
+//.........................Friend function.....................
+
+// A friend function is a non-member function that has access
+//to the private and protected members of a class.
+
+// #include <iostream>
+// using namespace std;
+
+// class Box {
+// private:
+//     int length;
+
+// public:
+//     Box(int l) {
+//         length = l;
+//     }
+
+//     // friend function declaration
+//     friend void printLength(Box b);
+// };
+
+// // Friend function definition
+// void printLength(Box b) {
+//     // ✔ Ab private member access allowed hai
+//     cout << "Length = " << b.length << endl;
+// }
+
+// int main() {
+//     Box b1(10);
+//     printLength(b1);
+//     return 0;
+// }
